40ad742f835889ab10a4c480c25568efafa415c1
Brian <bmccann36@gmail.com>
2018-05-21 14:00:12 -0400

    initial commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'YOUR_S3_UPLOADS_BUCKET_REGION',
+    BUCKET: 'YOUR_S3_UPLOADS_BUCKET_NAME'
+  },
+  apiGateway: {
+    REGION: 'YOUR_API_GATEWAY_REGION',
+    URL: 'YOUR_API_GATEWAY_URL'
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_FMZLWkYJB',
+    APP_CLIENT_ID: '1a0nbvhufpga8igrb7jjct949l',
+    IDENTITY_POOL_ID: 'YOUR_IDENTITY_POOL_ID'
+  }
+};
