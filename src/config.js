4826788c7b2173f19c3a7ea9b257f137a06dbf05
Brian McCann <bmccann36@gmail.com>
2018-06-11 22:59:33 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,8 +9,8 @@ export default {
   },
   cognito: {
     REGION: 'us-east-1',
-    USER_POOL_ID: 'us-east-1_FMZLWkYJB',
-    APP_CLIENT_ID: '1a0nbvhufpga8igrb7jjct949l',
+    USER_POOL_ID: '',
+    APP_CLIENT_ID: '',
     IDENTITY_POOL_ID: 'YOUR_IDENTITY_POOL_ID'
   }
 };
